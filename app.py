7bca11f498f51de9ce0f9ca44890ad1f4f14478c
142857 <84247106+CarlosDev314159@users.noreply.github.com>
2023-12-13 13:01:45 -0300

    Improved with GUI support after refactoring

--- app.py
+++ app.py
@@ -1,39 +1,110 @@
-import wget, os
-from tkinter import Tk, filedialog
-from colorama import Fore, init 
+import os
+import requests
+import PySimpleGUI as sg
+from termcolor import colored
+import random
+import sys
 
-# importing libs
 
-# To close another extra window opened by tkinter
-close_window = Tk()
-close_window.withdraw()
+# List of user agents to choose from randomly
+user_agents = [
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:53.0) Gecko/20100101 Firefox/53.0",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.1 Safari/603.1.30",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:53.0) Gecko/20100101 Firefox/53.0",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36 OPR/44.0.2510.1449",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36 Vivaldi/1.9.818.44",
+    "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.81 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:53.0) Gecko/20100101 Firefox/53.0",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; AS; rv:11.0) like Gecko",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.81 Safari/537.3",
+    "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:53.0) Gecko/20100101 Firefox/53.0",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.81 Safari/537.36 OPR/45.0.2552.635",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:52.0) Gecko/20100101 Firefox/52.0",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko",
+    "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.81 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.81 Safari/537.36 Vivaldi/1.9.818.44",
+    "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36 OPR/44.0.2510.1449"
+]
 
-# Remove coloring at the end of each print
-init(autoreset=True)
 
-# the list that will store the links
-images = []
+def show_message(message, icon_path):
+    return sg.popup(message, icon=icon_path)
 
-while True:
-    url = input("Press '0' to exit the program or Press '1' to go to downloads.\nPaste the link here:\n")
-    if url == "0":
-          exit()
-    elif url != "1":
-          images.append(url)
-    else:
-         path = filedialog.askdirectory(title="Where would you like to save?") # Open the directory to choose where to save
-         try:
-              if path == ():
-                 os.chdir(path) #switch to the chosen path
-         except TypeError as e:
-                 print(Fore.RED + "Canceled by user\n")
-                 exit()
-         try:
-             for link in images:
-                download = wget.download(link)
-                print("\n")
-                print(Fore.GREEN + "Download successful!")
-                exit()
-         except ValueError as e:
-                print(f"{Fore.RED}Download failed!\n{Fore.YELLOW}Error log: {e}")
-                exit()
+
+def download_image(link, path):
+    try:
+        sg.popup_timed(
+            f"Downloading {link}", auto_close_duration=500)
+        response = requests.get(
+            link, headers={'User-Agent': random.choice(user_agents)})
+        if response.status_code == 200:
+            with open(os.path.join(path, os.path.basename(link)), 'wb') as file:
+                file.write(response.content)
+            sg.popup_timed(f"Downloaded {link} with success in {path}",
+                           auto_close_duration=500)
+        else:
+            sg.popup_error(
+                f"Download failed!\nStatus code: {response.status_code}")
+    except Exception as e:
+        sg.popup_error("Download failed!")
+        sg.popup_error(f"ERROR LOG: {e}")
+
+
+def main():
+    images = []
+
+    layout = [
+        [sg.Text("Enter the image URL.")],
+        [sg.Input(key='-IN-')],
+        [sg.Button('OK'), sg.Button('Exit'), sg.Button('Download All')],
+    ]
+
+    window = sg.Window('Image Downloader', layout, icon=logo)
+
+    while True:
+        event, values = window.read()
+
+        if event == sg.WINDOW_CLOSED or event == 'Exit':
+            break
+        elif event == 'OK':
+            url = values['-IN-']
+            if url == "0":
+                break
+            elif url != "1" and url != "":
+                show_message(f"Added {url} to the list.", info_path)
+                images.append(url)
+                window['-IN-'].update('')
+            elif url == "1" and len(images) == 0:
+                show_message("No URL provided! Try again.", warning_path)
+            elif url == "":
+                show_message("No URL provided! Try again.", warning_path)
+            elif url == "1" and len(images) > 0:
+                path = sg.popup_get_folder('Where would you like to save?')
+                if path:
+                    os.chdir(path)
+                    for image in images:
+                        download_image(image, path)
+                else:
+                    show_message("No directory selected!", warning_path)
+                    break
+        elif event == 'Download All' and len(images) == 0:
+            show_message("No URL provided! Try again.", warning_path)
+        elif event == 'Download All' and len(images) > 0 and url != "":
+            path = sg.popup_get_folder('Where would you like to save?')
+            if path:
+                os.chdir(path)
+                for image in images:
+                    download_image(image, path)
+            else:
+                show_message("No directory selected!", warning_path)
+                break
+
+    window.close()
+
+
+if __name__ == "__main__":
+    main()
